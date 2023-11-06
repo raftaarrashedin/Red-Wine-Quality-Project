a49de0ee149b8ac5cf70884fe970bd5e00bf08fd
raftaar rashedin <raftaarrashedin100@gmail.com>
2023-11-06 17:27:17 +0530

    updated the pytest

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,16 @@
+import pytest
+
+class NotInRange(Exception) :
+	def __init__(self, message="value not in range") :
+		self.message = message
+		super().__init__(self.message)
 
 
-
-
+8080142595
 def test_generic() :
-	a = 2 
-	b = 2
-	assert a == b
+	a = 5
+	with pytest.raises(NotInRange) :
+		if a not in range(10,20) :
+			raise NotInRange
+	# b = 2
+	# assert a == b
