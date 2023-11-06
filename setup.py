42e6405e6289af98615a0208fcbb3f3fa292dc35
raftaar rashedin <raftaarrashedin100@gmail.com>
2023-11-06 14:20:41 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+	name="src",
+	version="0.0.1",
+	description="its a project to check the quality of red wine",
+	author="raftaarrashedin",
+	packages=find_packages(),
+	license="MIT"
+)
