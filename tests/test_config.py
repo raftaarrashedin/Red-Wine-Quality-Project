75bcad88d83e4a4df5c37682ea6c9da8d71dfa15
raftaar rashedin <raftaarrashedin100@gmail.com>
2023-11-10 21:31:40 +0530

    Test cases done

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,61 @@
 import pytest
+import json
+import logging
+import os
+import joblib
+from prediction_service.prediction import form_response, api_response
+import prediction_service
+
+
+input_data = {
+    "incorrect_range": 
+    {"fixed_acidity": 7897897, 
+    "volatile_acidity": 555, 
+    "citric_acid": 99, 
+    "residual_sugar": 99, 
+    "chlorides": 12, 
+    "free_sulfur_dioxide": 789, 
+    "total_sulfur_dioxide": 75, 
+    "density": 2, 
+    "pH": 33, 
+    "sulphates": 9, 
+    "alcohol": 9
+    },
+
+    "correct_range":
+    {"fixed_acidity": 5, 
+    "volatile_acidity": 1, 
+    "citric_acid": 0.5, 
+    "residual_sugar": 10, 
+    "chlorides": 0.5, 
+    "free_sulfur_dioxide": 3, 
+    "total_sulfur_dioxide": 75, 
+    "density": 1, 
+    "pH": 3, 
+    "sulphates": 1, 
+    "alcohol": 9
+    },
+
+    "incorrect_col":
+    {"fixed acidity": 5, 
+    "volatile acidity": 1, 
+    "citric acid": 0.5, 
+    "residual sugar": 10, 
+    "chlorides": 0.5, 
+    "free sulfur dioxide": 3, 
+    "total_sulfur dioxide": 75, 
+    "density": 1, 
+    "pH": 3, 
+    "sulphates": 1, 
+    "alcohol": 9
+    }
+}
+
+TARGET_range = {
+    "min": 3.0,
+    "max": 8.0
+}
+
 
 class NotInRange(Exception):
     def __init__(self, message="value not in range"):
@@ -10,3 +67,15 @@ def test_something():
     with pytest.raises(NotInRange):
         if a not in range(10, 20):
             raise NotInRange()
+
+
+
+
+
+def test_form_response_correct_range(data=input_data["correct_range"]):
+    res = form_response(data)
+    assert  TARGET_range["min"] <= res <= TARGET_range["max"]
+
+def test_api_response_correct_range(data=input_data["correct_range"]):
+    res = api_response(data)
+    assert  TARGET_range["min"] <= res["response"] <= TARGET_range["max"]
